d2ab33898f495edd7fbe7c68037aa66c7f807dc7
ritesh0033 <105968631+ritesh0033@users.noreply.github.com>
2023-07-02 21:11:18 +0545

    Add files via upload

---
+++ index,data.ja/data.js
@@ -0,0 +1,8 @@
+const data =[ {name:'ritesh',mail:'ry0323535@gmail.com'},
+{name:'shyam',mail:'ry033535@gmail.com'},
+{name:'rit',mail:'ry0323535@gmail.com'},
+{name:'ri',mail:'ry032535@gmail.com'},
+{name:'abcd',mail:'ry0323535@gmail.com'},
+{name:'abc',mail:'ry0323535@gmail.com'}
+]
+module.exports= data;
